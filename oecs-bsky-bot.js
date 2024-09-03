04f131135a860811f15ff50d6f8d3b4a0ee1c7dc
Mike Frank <mcfrank@stanford.edu>
2024-09-03 10:24:11 -0700

    every minute

--- oecs-bsky-bot.js
+++ oecs-bsky-bot.js
@@ -26,6 +26,6 @@ main();
 const scheduleExpressionMinute = '* * * * *'; // Run once every minute for testing
 const scheduleExpression = '0 */3 * * *'; // Run once every three hours in prod
 
-const job = new CronJob(scheduleExpression, main); // change to scheduleExpressionMinute for testing
+const job = new CronJob(scheduleExpressionMinute, main); // change to scheduleExpressionMinute for testing
 
 job.start();
